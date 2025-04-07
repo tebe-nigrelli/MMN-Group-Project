53946de565042b26f7ea2323e685aadcd21ad045
tferracina <tferracina@icloud.com>
2025-04-07 17:25:33 +0200

    add: EDA + get_dataset works with list of stimuli

--- utils.py
+++ utils.py
@@ -12,17 +12,24 @@ df_file = data_dir + "/" + "df.csv"
 manifest_path = os.path.join(data_dir, "manifest.json")
 cache = EcephysProjectCache.from_warehouse(manifest=manifest_path)
 
-def get_dataset(session_number:int, stimulus: str, regions:list):
+def get_dataset(session_number:int, stimuli: list, regions:list):
     session = cache.get_session_data(session_number)
-    table = session.get_stimulus_table(stimulus)
+
+    tables = [session.get_stimulus_table(stimulus) for stimulus in stimuli]
+    table = pd.concat(tables)
+
     partial_ids = session.units[session.units["ecephys_structure_acronym"].isin(regions)]
 
     spike_times_drifting = session.presentationwise_spike_times(
         stimulus_presentation_ids=table.index.values,
         unit_ids=partial_ids.index.values
     )
-    return spike_times_drifting.merge(table, left_on="stimulus_presentation_id", right_index=True) \
-           .merge(session.units, left_on="unit_id", right_index=True)
+    
+    return (
+        spike_times_drifting
+        .merge(table, left_on="stimulus_presentation_id", right_index=True)
+        .merge(session.units, left_on="unit_id", right_index=True)
+    )
            
 # df = get_dataset(750332458, "static_gratings", "VISam")
 # df.to_csv(df_file)
