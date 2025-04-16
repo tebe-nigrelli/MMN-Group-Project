e8858673722ec0d98b8e985bde9fb7547b71fe74
Thuna <thuna.cing@gmail.com>
2025-04-16 16:18:08 +0200

    temp.py: Polar orientation spike rate plot

--- temp.py
+++ temp.py
@@ -12,22 +12,26 @@ from constraints import *
 from dataset import *
 
 df_spike = get_spike_info(use_rates=True,
-                          stimulus_name=EQ('static_gratings'), # ISIN(['static_gratings', 'drifting_gratings']),
+                          stimulus_name=EQ('drifting_gratings'), # ISIN(['static_gratings', 'drifting_gratings']),
                           isi_violations=RANGE(None, 0.7))
 
-for region, df in df_spike.reset_index().groupby('structure_acronym'):
+df_groupby = df_spike.reset_index().groupby('structure_acronym')
+n = len(df_groupby)
+sup_fig = plt.figure()
+sup_fig.suptitle('Static Grating Unit Orientation Spike Rates')
+figs = sup_fig.subfigures(int(sqrt(n)), int(sqrt(n)), squeeze=False)
+for fig, (region, df) in zip(figs.flatten(), df_groupby):
   n = len(df['unit_id'].unique())
-  fig, axs = plt.subplots(int(sqrt(n)), int(sqrt(n)), squeeze=False,
-                          subplot_kw={"projection": "polar"},
-                          sharey=True)
+  axs = fig.subplots(int(sqrt(n)), int(sqrt(n)), squeeze=False,
+                     subplot_kw={"projection": "polar"},
+                     sharey=True)
   fig.suptitle(region)
   for ax, (unit_id, df) in zip(axs.flatten(), df.groupby('unit_id')):
-    ax.set_title(str(unit_id), size='small', y=0.95)
-    ax.set_xticks([0.0])
+    ax.set_title('')
+    ax.set_xticks([0.0], labels=[''])
+    ax.set_yticks([])
     df_data = filter_df(df.groupby('orientation')[['spike_mean']].mean().reset_index(),
                         FIELD(orientation=NOT(EQ('null'))))
     df_data['orientation'] *= np.pi / 180
-    ax.bar('orientation', 'spike_mean', width = np.pi * 15 / 180, data=df_data)
-  fig.savefig(f'orientation_spike_rates_{region}.svg')
-  break
-
+    ax.bar('orientation', 'spike_mean', width = np.pi * 45 / 180, data=df_data)
+sup_fig.savefig(f'orientation_spike_rates.svg')
